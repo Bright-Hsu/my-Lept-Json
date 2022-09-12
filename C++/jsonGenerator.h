c110dfa5595fe858a8a6ce4682836315b593e8c8
bright <2191579187@qq.com>
2022-09-12 19:24:25 +0800

    Create jsonGenerator.h

---
+++ C++/jsonGenerator.h
@@ -0,0 +1,26 @@
+#ifndef JSON_GENERATOR_H
+#define JSON_GENERATOR_H
+
+
+#include "jsonValue.h"
+
+namespace syo {
+
+    namespace json {
+
+        class Generator final{
+        public:
+            Generator(const Value& val, std::string &result);
+        private:
+            void stringify_value(const Value &v);
+            void stringify_string(const std::string &str);
+
+            std::string &res_;
+
+        };
+
+    }
+
+}
+
+#endif	//	JSON_GENERATOR_H
